70bd003b3f8c388f7014efed23b4cfb9737514b4
Tomi <tomas.casabona@gmail.com>
2024-08-25 22:11:00 +0200

    Add some bootstrap styles on card

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -3,23 +3,35 @@ import { useState } from "react";
 export const ItemCard = ({ imagen, titulo, descripcion, precio }) => {
   const [added, setAdded] = useState(false);
   return (
-    <div className="">
-      <img src={imagen} alt={titulo} className="tarjeta-imagen" />
-      <div className="tarjeta-contenido">
-        <h3 className="tarjeta-titulo"> {titulo} </h3>
-        <p className="tarjeta-descripcion">{descripcion}</p>
-        <p className="tarjeta-precio"> {precio}</p>
+    <div className="card mb-3 ">
+      <div className="row ">
+        <div className="col-3">
+          <img
+            src={imagen}
+            alt={titulo}
+            className="rounded p-4"
+            width={"250px"}
+            height={"250px"}
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">{titulo}</h5>
+            <p className="card-text">{descripcion}</p>
+            <p className="card-text fw-bolder fs-5">${precio}</p>
+            {added ? (
+              <button type="button" className="btn btn-danger">
+                Quitar
+              </button>
+            ) : (
+              <button type="button" className="btn btn-success">
+                {" "}
+                Agregar
+              </button>
+            )}
+          </div>
+        </div>
       </div>
-      {added ? (
-        <button type="button" className="boton-quitar">
-          quitar
-        </button>
-      ) : (
-        <button type="button" className="boton-agregar">
-          {" "}
-          agregar
-        </button>
-      )}
     </div>
   );
 };
