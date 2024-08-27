aa64528365be7bea6b96dccca3d0bd1afe06126e
Tomi <tomas.casabona@gmail.com>
2024-08-27 11:16:21 +0200

    Styles in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,34 +10,34 @@ export const NavBar = () => {
   const calculateItems = () => {
     return listaCompras.reduce((total, item) => total + item.cantidad, 0);
   };
+
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <NavLink to="/" className="navbar-brand">
-          Carrito
+    <nav className="navbar navbar-expand-lg bg-black border border-secondary rounded border-2 mb-3">
+      <div className="container-fluid ">
+        <NavLink to="/" className="navbar-brand text-light">
+          ShopApp
         </NavLink>
         <button
-          className="navbar-toggler"
-          type="button"
+          className="navbar-toggler-light d-block d-md-none "
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon text-light"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink to="/" className="nav-link active">
+              <NavLink to="/" className="nav-link active text-light">
                 Compras
               </NavLink>
             </li>
           </ul>
           <NavLink to="/carrito">
-            <Badge badgeContent={calculateItems()} color="secondary">
-              <ShoppingCart color="action" />
+            <Badge badgeContent={calculateItems()} color="error">
+              <ShoppingCart sx={{ color: "white" }} color="light" />
             </Badge>
           </NavLink>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,5 @@ createRoot(document.getElementById("root")).render(
     <StrictMode>
       <CarritoApp />
     </StrictMode>
-    ,
   </BrowserRouter>
 );
